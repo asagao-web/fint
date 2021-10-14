4fc4d29d3bdae436e93cc28f42ccbe1fd9937603
maya-win <maya@legaeng.com>
2021-10-14 19:17:42 +0900

    testing current profit included version

--- mod_bb.py
+++ mod_bb.py
@@ -1,4 +1,5 @@
 
+# from typing import type_check_only
 from OANDA_FUNC_CONF import *
 from datetime import datetime, timezone, timedelta
 import matplotlib.pyplot as plt
@@ -8,55 +9,69 @@ import random
 
 
 class Trade:
-    def __init__(self, arr):
-        self.position = 0
-        self.inPrice = 0
-        self.timeAfterOrder = 0
-        self.profit = 0
-        self.arr = arr
-        self.won = 0
-        self.lost = 0
-    def long(self,i,  ):
-        if self.position == 0:
-            self.inPrice = self.arr[i]
-            self.position = 1
-            self.timeAfterOrder = 0
-            # print(">>> LONG")
-        elif self.position == 1:
-            pass
-        elif self.position == -1:
-            self.close(i)
-    def short(self, i, ):
-        if self.position == 0:
-            self.inPrice = self.arr[i]
-            self.position = -1
-            self.timeAfterOrder = 0
-            # print(">>> SHORT")
-        elif self.position == 1:
-            self.close(i)
-        elif self.position == -1:
-            pass  
-    def close(self,i):
-        if self.position > 0:
-            profit = self.arr[i] - self.inPrice
-        elif self.position < 0:
-            profit = self.inPrice - self.arr[i]
-        else:
-            profit = None
-        if profit != None:
-            # print("profit: ", profit)
-            pass
-        else:
-            print("WARNING >>>>> DID NOT HAVE ANY POSITION BUT CLOSING...")
-        self.profit += profit
-        self.position = 0  
-        self.timeAfterOrder = 0
-        # update win ratio
-        if profit > 0:
-            self.won += 1
-        elif profit < 0:
-            self.lost += 1
-        # print("total profit: ", self.profit)
+	def __init__(self, arr):
+		self.position = 0
+		self.inPrice = 0
+		self.timeAfterOrder = 0
+		self.profit = 0
+		self.arr = arr
+		self.won = 0
+		self.lost = 0
+	def long(self,i,  ):
+		if self.position == 0:
+			self.inPrice = self.arr[i]
+			self.position = 1
+			self.timeAfterOrder = 0
+			# print(">>> LONG")
+		elif self.position == 1:
+			pass
+		elif self.position == -1:
+			self.close(i)
+	def short(self, i, ):
+		if self.position == 0:
+			self.inPrice = self.arr[i]
+			self.position = -1
+			self.timeAfterOrder = 0
+			# print(">>> SHORT")
+		elif self.position == 1:
+			self.close(i)
+		elif self.position == -1:
+			pass  
+	def close(self,i):
+		if self.position > 0:
+			profit = self.arr[i] - self.inPrice
+		elif self.position < 0:
+			profit = self.inPrice - self.arr[i]
+		else:
+			profit = None
+		if profit != None:
+			# print("profit: ", profit)
+			pass
+		else:
+			print("WARNING >>>>> DID NOT HAVE ANY POSITION BUT CLOSING...")
+		self.profit += profit
+		self.position = 0  
+		self.timeAfterOrder = 0
+		# update win ratio
+		if profit > 0:
+			self.won += 1
+		elif profit < 0:
+			self.lost += 1
+		# print("total profit: ", self.profit)
+		
+	def currentProfit(self, currentPrice):
+		currentProf = 0
+		if self.position == 1:
+			currentProf = currentPrice - self.inPrice
+		elif self.position == -1:
+			currentProf = self.inPrice - currentPrice
+		# update win ratio
+		if currentProf > 0:
+			self.won += 1
+		elif currentProf < 0:
+			self.lost += 1
+		return currentProf 
+
 
 def bbSimulate(SPAN, LENGTH, optW): # SPAN is oanda granularity
 	data = Data(SPAN, LENGTH)
@@ -96,7 +111,15 @@ def bbSimulate(SPAN, LENGTH, optW): # SPAN is oanda granularity
 			else:
 				pass
 		i += 1
-		
+	
+
+	# add unclosed situtation
+	row = bb.iloc[-1]
+	t.profit += t.currentProfit(row["raw"])
+
 	# print("************\n*************END") 
 	return round(t.profit,2), round(t.won / (t.won + t.lost),2)
 
+
+if __name__ == "__main__":
+	print(bbSimulate("H1", 2500, 170))
